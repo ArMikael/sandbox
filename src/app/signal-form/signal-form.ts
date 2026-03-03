c37ebdb04857bd33eaf26259758e67fa734a279f
Michael Treser <armikael2@gmail.com>
2026-03-03 13:09:28 +0200

    [SB-003]: feat(primeNg): Added PrimeNG lib to the project. Implemented basic text inputs and button from the lib.

--- src/app/signal-form/signal-form.ts
+++ src/app/signal-form/signal-form.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit, signal} from '@angular/core';
-import {form, FormField, minLength, required, debounce, min} from '@angular/forms/signals';
+import {form, FormField, minLength, required, debounce} from '@angular/forms/signals';
+import {InputText} from 'primeng/inputtext';
+import {ButtonDirective} from 'primeng/button';
+import {FormsModule} from '@angular/forms';
 
 interface BoardGame {
   title: string,
   publisher: string,
   designer: string,
   artist: string,
-  year: number,
+  year?: number,
 }
 
 @Component({
   selector: 'app-signal-form',
   imports: [
-    FormField
+    FormField,
+    InputText,
+    ButtonDirective,
+    FormsModule
   ],
+  providers: [],
   templateUrl: './signal-form.html',
   styleUrl: './signal-form.scss',
 })
@@ -21,7 +28,6 @@ export class SignalForm implements OnInit {
   boardGameModel = signal<BoardGame>({
     title: '',
     publisher: '',
-    year: 0,
     designer: '',
     artist: ''
   });
@@ -36,13 +42,11 @@ export class SignalForm implements OnInit {
 
     required(path.designer);
     minLength(path.designer, 2);
-
-    required(path.year);
-    min(path.year, 4);
   });
 
   ngOnInit() {
     this.boardGameForm.title().value.set('Oath');
+    this.boardGameForm.publisher().value.set('Leather Games');
     this.boardGameForm.designer().value.set('Cole Wehrle');
   }
 
