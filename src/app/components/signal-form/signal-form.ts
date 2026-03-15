805560e8c0b45ba7529833dd29172fb1aaee6be5
Michael Treser <armikael2@gmail.com>
2026-03-15 20:45:33 +0200

    [SB-008]: feat(signal form): Signal form updated to use schema. Added support custom error message.

--- src/app/components/signal-form/signal-form.ts
+++ src/app/components/signal-form/signal-form.ts
@@ -1,9 +1,17 @@
-import {Component, OnInit, signal} from '@angular/core';
-import {form, FormField, minLength, required, debounce} from '@angular/forms/signals';
-import {InputText} from 'primeng/inputtext';
-import {ButtonDirective} from 'primeng/button';
-import {FormsModule} from '@angular/forms';
-import {BoardGame} from '../../models/interfaces';
+import { Component, OnInit, signal } from '@angular/core';
+import {form, FormField, minLength, required, debounce, Schema, schema, apply} from '@angular/forms/signals';
+import { InputText } from 'primeng/inputtext';
+import { ButtonDirective } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { BoardGame } from '../../models/interfaces';
+
+
+// We can define all validators and messages as a schema to apply it on multiple fields at the same time.
+const textInputSchema: Schema<string> = schema<string>((path) => {
+  required(path, { message: 'This field is required!' });
+  minLength(path, 3, { message: 'The value is too short!' });
+});
+
 
 @Component({
   selector: 'app-signal-form',
@@ -31,11 +39,11 @@ export class SignalForm implements OnInit {
     minLength(path.title, 3);
     debounce(path.title, 500);
 
-    required(path.publisher);
-    minLength(path.publisher, 2);
+    required(path.publisher, { message: 'Publisher is a required field.' });
+    minLength(path.publisher, 2, { message: 'Publisher length should be at least 2 characters.' });
 
-    required(path.designer);
-    minLength(path.designer, 2);
+    apply(path.designer, textInputSchema);
+    apply(path.artist, textInputSchema);
   });
 
   ngOnInit() {
